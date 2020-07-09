899769d5a9c137d0f26eed0324723dba8705b19c
rowaxl <rowaxl0@gmail.com>
2020-07-08 20:45:00 -0700

    add getTotalAmount function

--- practice/invoice/Invoice.java
+++ practice/invoice/Invoice.java
@@ -4,47 +4,52 @@ public class Invoice {
   private ArrayList<LineItem> items;
   private Address billingAddress;
 
-  public Invoice() {
+  public Invoice(Address address) {
     this.items = new ArrayList<LineItem>();
+    this.billingAddress = address;
   }
 
   public void addItem(Product product, int quantity) {
     this.items.add(new LineItem(product, quantity));
   }
 
-  public void setBillingAddress(Address address) {
-    this.billingAddress = address;
+  public double getTotalAmount() {
+    double totalAmount = 0;
+
+    for (LineItem item : this.items) {
+      totalAmount += item.getTotalPrice();
+    }
+
+    return totalAmount;
   }
 
   public String toString() {
     String invoiceFormat = "\n          I N V O I C E\n\n%s\n\nDescription          Price  Qty  Total\n\n%s\nAMOUNT DUE: $%.2f";
 
     String stringLineItem = "";
-    double totalAmount = 0;
+
     for (LineItem item: this.items) {
       stringLineItem += item.format() + "\n";
-      totalAmount += item.getTotalPrice();
     }
 
     return String.format(
         invoiceFormat,
         this.billingAddress.getAddress(),
         stringLineItem,
-        totalAmount
+        this.getTotalAmount()
       );
   }
 
   public static void main(String[] args) {
-    Invoice i1 = new Invoice();
-
     Address address1 = new Address(
       "Kim's Convenience",
       "100 West Broadway",
       "Vancouver",
       "BC", 
       "V5M 1A1"
-      );
-    i1.setBillingAddress(address1);
+    );
+
+    Invoice i1 = new Invoice(address1);
 
     Product toaster = new Product("Toaster", 29.95);
     i1.addItem(toaster, 3);
