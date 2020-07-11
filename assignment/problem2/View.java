f3c4d53c9ad1a752d8bab826b2b067f4fcc77230
rowaxl <rowaxl0@gmail.com>
2020-07-10 19:07:56 -0700

    add pattern

--- assignment/problem2/View.java
+++ assignment/problem2/View.java
@@ -19,14 +19,18 @@ public class View {
     View v = new View();
 
     StudentData s1 = new StudentData("Ali", "David", 80);
-    StudentData s2 = new StudentData("Ana", "Leu", 82);
-    StudentData s3 = new StudentData("Donald", "Trump", 81);
-    StudentData s4 = new StudentData("Peter", "Jackson", 79);
-    StudentData s5 = new StudentData("William", "Chen", 76);
-    StudentData s6 = new StudentData("Bob", "Wes", 74);
+    StudentData s2 = new StudentData("Peter", "Jackson", 79);
+    StudentData s3 = new StudentData("William", "Chen", 76);
+    StudentData s4 = new StudentData("Donald", "Trump", 81);
+    StudentData s5 = new StudentData("Bob", "Wes", 74);
+    StudentData s6 = new StudentData("Ana", "Leu", 82);
 
     List<StudentData> students = List.of(s1, s2, s3, s4, s5, s6);
     v.setStudents(students);
+    
+    List<StudentRow> wholeStudents = v.getStudents().stream()
+        .map((StudentData data) -> new StudentRow(data))
+        .collect(Collectors.toList());
 
     List<StudentRow> gradeAStudents = v.getStudents().stream()
         .filter(student -> student.gpa >= 80)
@@ -47,5 +51,11 @@ public class View {
     Table classB = new Table<StudentRow>("Class Grade: B");
     classB.rows = gradeBStudents;
     classB.print();
+
+    System.out.println();
+
+    Table school = new Table<StudentRow>("School");
+    school.rows = wholeStudents;
+    school.print();
   }
 }
